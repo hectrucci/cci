bcfdfdfd2840981935ed2ac89beb2624dea98162
hectrucci <hectrucci@gmail.com>
2018-04-02 21:26:22 +0000

    Chapter1 - isUnique & checkPermutation

---
+++ chapter_1_arrays_and_strings/1.1.is.unique.js
@@ -0,0 +1,15 @@
+const isUnique = str => {
+    const chars = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str.charAt(i);
+
+        if (chars[currentChar]) {
+            return false;
+        }
+
+        chars[currentChar] = currentChar;
+    }
+
+    return true;
+};
---
+++ chapter_1_arrays_and_strings/1.2.check.permutation.js
@@ -0,0 +1,28 @@
+const checkPermutation = (str1, str2) => {
+    if (str1.length === str2.length) {
+        const charsOfStr1 = getCharSet(str1);
+        const charsOfStr2 = getCharSet(str2);
+
+        for (let currentChar in charsOfStr1) {
+
+            // will omit the hasOwnProperty check >:)
+            if (charsOfStr1[currentChar] !== charsOfStr2[currentChar]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+};
+
+const getCharSet = str => {
+    const charSet = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+
+        charSet[currentChar] = (charSet[currentChar] || 0) + 1;
+    }
+
+    return charSet;
+};
