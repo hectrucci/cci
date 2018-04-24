87069bc7523b40810a518f799cce9d79bc306c38
hectrucci <hectrucci@gmail.com>
2018-04-24 14:50:33 +0000

    Three In One

--- src/chapter_3_stacks_and_queues/3.1.three.in.one.js
+++ src/chapter_3_stacks_and_queues/3.1.three.in.one.js
@@ -1,7 +1,11 @@
 class MultiStack {
-    constructor() {
+    constructor(stackNum = 3) {
         this._stack = [];
-        this._lengths = [0, 0, 0];
+        this._lengths = [];
+
+        for (let i = 0; i < stackNum; i++) {
+            this._lengths.push(0);
+        }
     }
 
     _getLength(stackNum) {
@@ -20,6 +24,8 @@ class MultiStack {
     }
 
     pop(stackNum) {
+        if (!stackNum || stackNum > this._lengths.length) return null;
+
         const index = this._getIndex(stackNum);
         this._lengths[stackNum - 1] = this._lengths[stackNum - 1] - 1;
 
@@ -27,6 +33,8 @@ class MultiStack {
     }
 
     push(stackNum, item) {
+        if (!stackNum || stackNum > this._lengths.length) return null;
+
         const index = this._getIndex(stackNum);
 
         this._stack.splice(index, 0, item);
@@ -34,11 +42,15 @@ class MultiStack {
     }
 
     peek(stackNum) {
+        if (!stackNum || stackNum > this._lengths.length) return null;
+
         const index = this._getIndex(stackNum);
         return this._stack[index - 1];
     }
 
     isEmpty(stackNum) {
+        if (!stackNum || stackNum > this._lengths.length) return null;
+
         return !this._lengths[stackNum - 1];
     }
 }
--- src/chapter_3_stacks_and_queues/3.1.three.in.one.spec.js
+++ src/chapter_3_stacks_and_queues/3.1.three.in.one.spec.js
@@ -5,7 +5,7 @@ describe('Three In One', () => {
     let multiStack;
 
     beforeEach(() => {
-        multiStack = new MultiStack();
+        multiStack = new MultiStack(3);
 
         multiStack.push(1, 1);
         multiStack.push(1, 1);
@@ -16,6 +16,20 @@ describe('Three In One', () => {
         multiStack.push(1, 4);
         multiStack.push(2, 5);
         multiStack.push(3, 6);
+    });
+
+    it('should push each stack', () => {
+        multiStack.push(1, 7);
+        multiStack.push(2, 8);
+        multiStack.push(3, 9);
+
+        expect(multiStack.peek(1)).to.equal(7);
+        expect(multiStack.peek(2)).to.equal(8);
+        expect(multiStack.peek(3)).to.equal(9);
+
+        expect(multiStack._lengths[0]).to.equal(4);
+        expect(multiStack._lengths[1]).to.equal(4);
+        expect(multiStack._lengths[2]).to.equal(4);
     });
 
     it('should peek the 3 stacks', () => {
@@ -32,5 +46,11 @@ describe('Three In One', () => {
         expect(multiStack._lengths[0]).to.equal(2);
         expect(multiStack._lengths[1]).to.equal(2);
         expect(multiStack._lengths[2]).to.equal(2);
+    });
+
+    it('should not have empty stacks', () => {
+        expect(multiStack.isEmpty(1)).to.equal(false);
+        expect(multiStack.isEmpty(2)).to.equal(false);
+        expect(multiStack.isEmpty(3)).to.equal(false);
     });
 });
